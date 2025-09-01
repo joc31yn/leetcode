014fd87e6ca8b15f856e350e1dc287eb194f0b90
joc31yn <joce.xu06@gmail.com>
2025-08-31 23:28:05 -0400

    improve time complexity

--- blind-75/contains_duplicate.py
+++ blind-75/contains_duplicate.py
@@ -2,11 +2,14 @@ from typing import List
 
 
 def has_duplicate(nums: List[int]) -> bool:
-    seen = []
+    """
+    Time Complexity: O(n) with hashset
+    """
+    seen = set()
     for num in nums:
         if num in seen:
             return True
-        seen.append(num)
+        seen.add(num)
     return False
 
 

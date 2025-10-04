cc693be0aa504b0e1e909324b09f160e075a9901
joc31yn <joce.xu06@gmail.com>
2025-10-04 17:33:29 -0400

    valid palindrome

---
+++ blind-75/valid_palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def is_palindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+                continue
+            if not s[end].isalnum():
+                end -= 1
+                continue
+            if s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
+
+
+sol = Solution()
+print(sol.is_palindrome("rac;;12321car"))

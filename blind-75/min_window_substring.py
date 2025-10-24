17be8fb4163b780b9cc53403794cc0b9e8a8ec46
joc31yn <joce.xu06@gmail.com>
2025-10-23 21:50:30 -0400

    optimal sol for min window sub

--- blind-75/min_window_substring.py
+++ blind-75/min_window_substring.py
@@ -44,48 +44,85 @@
 #         return min_sub
 
 
+# class Solution2(object):
+#     def minWindow(self, s, t):
+#         """
+#         :type s: str
+#         :type t: str
+#         :rtype: str
+#         Better sol - still not too good
+#         Time complexity is O(n) but recopy min_sub each time a new sol
+#         is found is slow, so store indexes of min_sub and copy string last,
+#         following is (done in optimal sol)
+#         """
+#         freq_t = {}
+#         for letter in t:
+#             freq_t[letter] = 1 + freq_t.get(letter, 0)
+#         need = len(freq_t)
+#         have = 0
+#         min_sub = ""
+#         freq_s = {}
+#         start, end = 0, 0
+#         same_end = False
+#         while start < len(s) and end < len(s):
+#             if s[start] not in freq_t:
+#                 start += 1
+#                 if start > end:
+#                     end = start
+#                 continue
+
+#             if s[end] in freq_t:
+#                 freq_s[s[end]] = 1 + freq_s.get(s[end], 0)
+#                 if same_end:
+#                     freq_s[s[end]] -= 1
+#                 if freq_s[s[end]] == freq_t[s[end]] and not same_end:
+#                     have += 1
+#                 if have == need:
+#                     if min_sub == "" or len(min_sub) > end - start + 1:
+#                         min_sub = s[start: end + 1]
+#                     freq_s[s[start]] -= 1
+#                     if freq_s[s[start]] < freq_t[s[start]]:
+#                         have -= 1
+#                     start += 1
+#                     same_end = True
+#                 else:
+#                     same_end = False
+#                     end += 1
+#             else:
+#                 same_end = False
+#                 end += 1
+#         return min_sub
+
+
 class Solution(object):
     def minWindow(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: str
-        Better sol - still not too good
+        Time Complexity: O(n)
+        - optimal solution
         """
         freq_t = {}
         for letter in t:
             freq_t[letter] = 1 + freq_t.get(letter, 0)
         need = len(freq_t)
         have = 0
-        min_sub = ""
         freq_s = {}
         start, end = 0, 0
-        same_end = False
-        while start < len(s) and end < len(s):
-            if s[start] not in freq_t:
+        res_window, res_len = [-1, -1], 0
+        while end < len(s):
+            letter = s[end]
+            freq_s[letter] = 1 + freq_s.get(letter, 0)
+            if letter in freq_t and freq_s[letter] == freq_t[letter]:
+                have += 1
+            while have == need:
+                if res_len == 0 or res_len > end - start + 1:
+                    res_window = [start, end + 1]
+                    res_len = end - start + 1
+                freq_s[s[start]] -= 1
+                if s[start] in freq_t and freq_s[s[start]] < freq_t[s[start]]:
+                    have -= 1
                 start += 1
-                if start > end:
-                    end = start
-                continue
-
-            if s[end] in freq_t:
-                freq_s[s[end]] = 1 + freq_s.get(s[end], 0)
-                if same_end:
-                    freq_s[s[end]] -= 1
-                if freq_s[s[end]] == freq_t[s[end]] and not same_end:
-                    have += 1
-                if have == need:
-                    if min_sub == "" or len(min_sub) > end - start + 1:
-                        min_sub = s[start: end + 1]
-                    freq_s[s[start]] -= 1
-                    if freq_s[s[start]] < freq_t[s[start]]:
-                        have -= 1
-                    start += 1
-                    same_end = True
-                else:
-                    same_end = False
-                    end += 1
-            else:
-                same_end = False
-                end += 1
-        return min_sub
+            end += 1
+        return s[res_window[0]: res_window[1]] if res_len > 0 else ""

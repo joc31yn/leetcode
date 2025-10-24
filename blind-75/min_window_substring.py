75aed2a2703150ee27ca930a545cb2b2055bef2d
joc31yn <joce.xu06@gmail.com>
2025-10-23 20:48:06 -0400

    better sol to min window sub

--- blind-75/min_window_substring.py
+++ blind-75/min_window_substring.py
@@ -42,3 +42,50 @@
 #             else:
 #                 end += 1
 #         return min_sub
+
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        Better sol - still not too good
+        """
+        freq_t = {}
+        for letter in t:
+            freq_t[letter] = 1 + freq_t.get(letter, 0)
+        need = len(freq_t)
+        have = 0
+        min_sub = ""
+        freq_s = {}
+        start, end = 0, 0
+        same_end = False
+        while start < len(s) and end < len(s):
+            if s[start] not in freq_t:
+                start += 1
+                if start > end:
+                    end = start
+                continue
+
+            if s[end] in freq_t:
+                freq_s[s[end]] = 1 + freq_s.get(s[end], 0)
+                if same_end:
+                    freq_s[s[end]] -= 1
+                if freq_s[s[end]] == freq_t[s[end]] and not same_end:
+                    have += 1
+                if have == need:
+                    if min_sub == "" or len(min_sub) > end - start + 1:
+                        min_sub = s[start: end + 1]
+                    freq_s[s[start]] -= 1
+                    if freq_s[s[start]] < freq_t[s[start]]:
+                        have -= 1
+                    start += 1
+                    same_end = True
+                else:
+                    same_end = False
+                    end += 1
+            else:
+                same_end = False
+                end += 1
+        return min_sub

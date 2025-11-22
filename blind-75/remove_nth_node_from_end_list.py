65fd4582b8831c655f6160078d15f3a36c107bbb
Jocelyn Xu <joce.xu06@gmail.com>
2025-11-22 13:12:41 -0500

    add 2 pointer

--- blind-75/remove_nth_node_from_end_list.py
+++ blind-75/remove_nth_node_from_end_list.py
@@ -30,3 +30,21 @@ class Solution:
         prev.next = start.next
         start.next = None
         return head
+
+
+# class Solution2:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         """
+#         Two pointer method
+#         """
+#         fast = head
+#         slow = ListNode(0, head)
+#         temp = slow
+#         while n > 0:
+#             fast = fast.next
+#             n -= 1
+#         while fast:
+#             fast = fast.next
+#             slow = slow.next
+#         slow.next = slow.next.next
+#         return temp.next
